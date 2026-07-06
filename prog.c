01823a42abdde4989b38e669af78e195df417f20
subashini15-ram <subashini.ram2015@gmail.com>
2026-07-06 19:21:23 +0530

    added second point

---
+++ prog.c
@@ -0,0 +1,15 @@
+#include<studio.h>
+int main()
+{
+       int price,discount,gst,total; 
+       printf("enter product price:");
+       scanf("%p", &price);
+       printf("enter the discount percentage:");
+       scanf("%d", &discount);
+       printf("%f", &gst);
+
+    price= price-(price*discount/100);
+    total=price+(price*gst/100);
+    printf("final amount to pay: total");
+    return 0;
+}
